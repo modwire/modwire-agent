458b5f250593d21cdbe4a8e168c9c9e4cb6d96d8
Tomasz Szpak <tomszp@gmail.com>
2026-07-19 13:43:40 +0200

    update

--- src/modwire/core/siren.py
+++ src/modwire/core/siren.py
@@ -2,10 +2,12 @@ from functools import cache
 from typing import Any
 
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from modwire_siren import ModwireSirenFactory, SirenResourceSpec, inject_siren_resources
 from modwire_siren.integrations.django import to_django_response
 from modwire_siren.integrations.ninja_extra import NinjaExtraSirenResponse
+from modwire_siren.standards import SirenMediaType
+from ninja.errors import HttpError
 from ninja_extra import NinjaExtraAPI
 
 from .siren_module import SirenModule
@@ -28,6 +30,7 @@ class SirenApi:
                 docs_url=None,
             )
             cls._api = NinjaExtraAPI(**configuration)
+            cls._api.add_exception_handler(HttpError, siren_http_error_response)
             cls._api.register_controllers(SirenRootController, *cls.controllers())
         return cls._api
 
@@ -39,19 +42,29 @@ class SirenApi:
         return languages, records
 
     @classmethod
-    def resources(cls) -> tuple[SirenResourceSpec, ...]: return tuple(resource for module in cls.modules() for resource in module.resources)
+    def resources(cls) -> tuple[SirenResourceSpec, ...]:
+        return tuple(resource for module in cls.modules() for resource in module.resources)
 
     @classmethod
     def controllers(cls): return tuple(controller for module in cls.modules() for controller in module.controllers)
 
     @classmethod
-    def project(cls, request: Any): return ModwireSirenFactory.web(cls.schema(), base_url_resolver=lambda value: value.build_absolute_uri("/")).for_request(request)
+    def project(cls, request: Any):
+        return ModwireSirenFactory.web(
+            cls.schema(),
+            base_url_resolver=lambda value: value.build_absolute_uri("/"),
+        ).for_request(request)
 
     @classmethod
     def schema(cls) -> dict[str, Any]: return inject_siren_resources(cls.api().get_openapi_schema(), cls.resources())
 
     @staticmethod
-    def response(document: dict[str, Any]) -> HttpResponse: return to_django_response(NinjaExtraSirenResponse(body=document))
+    def response(document: dict[str, Any]) -> HttpResponse:
+        return to_django_response(NinjaExtraSirenResponse(body=document, content_type=SirenMediaType.ENTITY.value))
+
+
+def siren_http_error_response(request: HttpRequest, error: HttpError) -> JsonResponse:
+    return JsonResponse({"detail": str(error)}, status=error.status_code)
 
 
 SirenNinja = SirenApi
