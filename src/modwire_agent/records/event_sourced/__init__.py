6fe7466dc68f74bdf8bbcb0ee4d31a215a8da1bd
Tomasz Szpak <tomszp@gmail.com>
2026-07-29 12:17:58 +0200

    remove es demo

--- src/modwire_agent/records/event_sourced/__init__.py
+++
@@ -1,3 +0,0 @@
-from .application import RecordsApplication
-
-__all__ = ["RecordsApplication"]
--- src/modwire_agent/records/event_sourced/system.py
+++
@@ -1,6 +0,0 @@
-from eventsourcing.system import System
-
-from .application import RecordsApplication
-from .projections import RecordsProjection
-
-records_system = System([[RecordsApplication, RecordsProjection]])
